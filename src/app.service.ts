39d5c808e0e7817e7e016d14f4eae7f3abd7cf2e
fatuxx <facuotermin@gmail.com>
2023-01-17 00:25:19 +1100

    Exceptions control, variables name and parseInt

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,73 +7,73 @@ export class AppService {
   constructor(private readonly httpService: HttpService) { }
   public logger = new Logger(AppService.name);
 
-  public compareInvoiceAndPo(): void {
+  public async compareInvoiceAndPo(): Promise<void> {
 
     const axios = this.httpService.axiosRef;
 
     this.logger.log("getting invoices from the API...");
-    let invoices = axios.get(' https://dh7aht0kba.execute-api.ap-southeast-2.amazonaws.com/dev/invoices')
-      .then((response) => {
-        let jsonInvoices = response.data
+    const invoices = await axios.get('https://dh7aht0kba.execute-api.ap-southeast-2.amazonaws.com/dev/invoices');
+    let jsonInvoices = invoices.data;
 
-        Logger.log("iterating array of invoices...");
+    Logger.log("iterating over the array of invoices...");
+    jsonInvoices.body.forEach(async function (invoice) {
 
-        jsonInvoices.body.forEach(function (invoice) {
+      Logger.log("getting the PO_NUMBER from the invoice...");
+      let po_number = invoice.Fields.find(i => i.FieldName == "PO_NUMBER")
+      try{  
+        
+        //check if there is an order
+        if (!po_number){
+          throw new Error("ERRORMESAGE_PURCHARSE_ORDER_NOT_FOUND")
+        }
+          Logger.log("PO_NUMBER: " + po_number.Item);
+          Logger.log("getting the related Po from the API by PO_NUMBER...");
+          const purchase_order= await axios.get('https://dh7aht0kba.execute-api.ap-southeast-2.amazonaws.com/dev/purchase-orders/' + po_number.Item)
+          
+          //List of pruducts (lines/rows) in the invoice
+          let listOfItems = invoice.Fields.find(items => items.FieldName == "ITEMS").Item.Row
+          
+          //Iterating list of products
+          listOfItems.forEach(async function (row) {
+          
+            let codeProductInvoice = row.ColumnValue[0].Item
+            let jsonPO = purchase_order.data[0]
 
-          Logger.log("getting the PO_NUMBER from the invoice...\n");
+            Logger.log("Comparing product with code: " + codeProductInvoice);
+            let poProductFound = jsonPO.POPurchaseOrder.LineItems.find(codeProductPO =>
+            codeProductPO.StockItem == codeProductInvoice ||
+            codeProductPO.SupplierPartNo == codeProductInvoice)
+            try
+            {
+              if (!poProductFound) {
+                throw new Error("ERRORMESAGE_CODE_MISSMATCH")
+              }
+              Logger.log("Items code " + codeProductInvoice + " match!");
+              Logger.log("Comparing quantities...");
 
-          let po_number = invoice.Fields.find(i => i.FieldName == "PO_NUMBER")
+              let quantityProductInvoice = parseInt(row.ColumnValue[4].Item);
 
-          //check if there is an order
-          if (po_number != null) {
+              Logger.log("PO product quantity: " + poProductFound.Quantity);
+              Logger.log("Invoice product quantity: " + quantityProductInvoice);
 
-            Logger.log("PO_NUMBER: " + po_number.Item);
-            Logger.log("getting the related Po from the API by PO_NUMBER...");
-
-            axios.get('https://dh7aht0kba.execute-api.ap-southeast-2.amazonaws.com/dev/purchase-orders/' + po_number.Item)
-              .then((response) => {
-
-                //List of pruducts (lines/rows) in the invoice
-                let listOfItems = invoice.Fields.find(items => items.FieldName == "ITEMS").Item.Row
-
-                //Iterating list of products
-                listOfItems.forEach((function (row) {
-
-                  let codeProductInvoice = row.ColumnValue[0].Item
-                  let jsonPO = response.data[0]
-
-                  Logger.log("Comparing product with code: " + codeProductInvoice);
-
-                  let POproductFound = jsonPO.POPurchaseOrder.LineItems.find(codeProductPO =>
-                    codeProductPO.StockItem == codeProductInvoice ||
-                    codeProductPO.SupplierPartNo == codeProductInvoice)
-
-                  if (POproductFound != null) {
-                    Logger.log("Items code " + codeProductInvoice + " match!");
-                    Logger.log("Comparing quantities...");
-
-                    let quantityProductInvoice = row.ColumnValue[4].Item
-                    Logger.log("PO product quantity: " + POproductFound.Quantity);
-                    Logger.log("Invoice product quantity: " + parseInt(quantityProductInvoice));
-                    if (POproductFound.Quantity == quantityProductInvoice) {
-                      Logger.log("Quantities match too!\n");
-                    }
-                    else {
-                      Logger.log("Quantities missmatch!\n");
-                    }
-                  }
-                  else {
-                    Logger.log("Items code " + codeProductInvoice + " missmatch!\n");
-                  }
-                }))
-              });
-          }
-          else {
-            Logger.log("PO_Number not found/n");
-          }
-        });
-      });
-
+              if (poProductFound.Quantity !== quantityProductInvoice) {
+                throw new Error("ERRORMESAGE_QUANTITIES_MISSMATCH")
+              }
+              else {
+                Logger.log("Quantities match too!\n");
+              }
+            } catch(error){
+                ErrorHandler(error)
+            }
+          })
+      }catch(error){
+        ErrorHandler(error)
+      }
+    })
   }
+}
+
+function ErrorHandler(error: any) {
+  Logger.log("Error in compareInvoiceAndPo: "+ error.message);
 }
 
